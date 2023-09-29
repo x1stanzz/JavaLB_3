05bfca65973f5e0ac3064ba766b08187bbd8b199
x1stanzz <vova28032003@gmail.com>
2023-09-29 15:52:04 +0300

    Created an entity named Order with its attributes

---
+++ src/main/java/org/example/Order.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Order {
+    private int orderId;
+    private ArrayList<Product> products = new ArrayList<Product>();
+    private String status;
+
+    public Order(int orderId, ArrayList<Product> products, String status){
+        this.orderId = orderId;
+        this.products = products;
+        this.status = status;
+    }
+
+}
