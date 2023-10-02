cb55d2aab22317d0349d356eac2141d23623f9c1
x1stanzz <vova28032003@gmail.com>
2023-10-02 20:38:19 +0300

    Created a method to set status

--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -5,15 +5,21 @@ import java.util.ArrayList;
 public class Order {
     private int orderId;
     private ArrayList<Product> products = new ArrayList<Product>();
-    private String status;
+    private String status = "Pending";
 
-    public Order(int orderId, ArrayList<Product> products, String status){
+    public Order(int orderId, ArrayList<Product> products){
         this.orderId = orderId;
         this.products = products;
-        this.status = status;
     }
 
     public String getStatus(){
         return this.status;
     }
+
+    public void placeOrder(){
+        if(!products.isEmpty()){
+            status = "Placed";
+        }
+    }
+
 }
